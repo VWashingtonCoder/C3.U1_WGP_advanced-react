f3d9ab72243f3047c2056e0c0f2c1acb7c297adc
Ladrillo <gabriel@cabrejas.net>
2022-02-07 20:11:55 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -57,7 +57,7 @@ export default class ClassComponent extends React.Component {
   onDelete = id => event => {
     axios.delete(`${URL}/${id}`)
       .then(res => {
-        this.setState({ ...this.state, quotes: [] })
+        this.setState({ ...this.state, quotes: ??? })
       })
       .catch(this.onError)
     // only frontend hack
