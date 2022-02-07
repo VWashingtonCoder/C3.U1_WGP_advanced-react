54bbb437ac9e506ed1f60017b3ff964e6c82bdde
Ladrillo <gabriel@cabrejas.net>
2022-02-07 19:18:15 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -43,8 +43,8 @@ export default class ClassComponent extends React.Component {
           }
         </ul>
         <form>
-          <input onChange={} value={this.state.textInput} type="text" id="text" placeholder="type text" />
-          <input onChange={} value={this.state.authorInput} type="text" id="author" placeholder="type author" />
+          <input onChange={this.onChange} value={this.state.textInput} type="text" id="text" placeholder="type text" />
+          <input onChange={this.onChange} value={this.state.authorInput} type="text" id="author" placeholder="type author" />
           <input type="submit" />
         </form>
       </div>
