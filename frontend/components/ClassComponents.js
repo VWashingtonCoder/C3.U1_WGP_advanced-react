7be97fbdcdd12750266cab4b88c197e69cb6d0d1
Ladrillo <gabriel@cabrejas.net>
2022-02-07 19:56:02 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -39,7 +39,7 @@ export default class ClassComponent extends React.Component {
     const payloadToSend = { author: this.state.authorInput, text: this.state.textInput }
     axios.post(URL, payloadToSend)
       .then(res => {
-        this.setState({ ...this.state, quotes: res.data.quotes })
+        this.setState({ ...this.state, quotes: res.data.quotes }) // do not copy & paste this code
       })
       .catch(err => {
         const errorFromAPI = err.response.data.message
