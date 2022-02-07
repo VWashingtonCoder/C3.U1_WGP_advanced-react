ce5619a940bf0daba28560eeea0bfac8c207bf65
Ladrillo <gabriel@cabrejas.net>
2022-02-07 20:19:47 +0100

    committing to "lecture"

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default class QuoteForm extends React.Component {
   render() {
     return (
-
+      <form onSubmit={this.props.onSubmit}>
+        <input onChange={this.onChange} value={this.state.textInput} type="text" id="textInput" placeholder="type text" />
+        <input onChange={this.onChange} value={this.state.authorInput} type="text" id="authorInput" placeholder="type author" />
+        <input type="submit" />
+      </form>
     )
   }
 }
