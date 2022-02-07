dd7d2ae523ed9d809edbc0fe1423b6513c6d1d95
Ladrillo <gabriel@cabrejas.net>
2022-02-07 20:20:46 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import Form from './'
 
 const initialState = {
   quotes: [{ author: 'Gabe', text: 'Do not troll Gabe', id: 'xyz' }],
--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -4,8 +4,8 @@ export default class QuoteForm extends React.Component {
   render() {
     return (
       <form onSubmit={this.props.onSubmit}>
-        <input onChange={this.onChange} value={this.state.textInput} type="text" id="textInput" placeholder="type text" />
-        <input onChange={this.onChange} value={this.state.authorInput} type="text" id="authorInput" placeholder="type author" />
+        <input onChange={this.props.onChange} value={this.props.textInput} type="text" id="textInput" placeholder="type text" />
+        <input onChange={this.props.onChange} value={this.props.authorInput} type="text" id="authorInput" placeholder="type author" />
         <input type="submit" />
       </form>
     )
