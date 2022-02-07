f386243212b48ea9675c961f404771d543f06bb1
Ladrillo <gabriel@cabrejas.net>
2022-02-07 19:16:50 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -44,7 +44,7 @@ export default class ClassComponent extends React.Component {
         </ul>
         <form>
           <input value={this.state.textInput} type="text" id="text" placeholder="type text" />
-          <input value={} type="text" id="author" placeholder="type author" />
+          <input value={this.state.authorInput} type="text" id="author" placeholder="type author" />
           <input type="submit" />
         </form>
       </div>
