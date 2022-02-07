02b8f377a957c7cc733a78b6f03c187186c35af6
Ladrillo <gabriel@cabrejas.net>
2022-02-07 14:57:31 +0100

    prep

--- README.md
+++ README.md
@@ -1,20 +1,17 @@
-# Codegraded Project React
+# Advanced React Guided Projects
 
 ## Scripts
 
 - `npm install`
 - `npm run dev`
-- `npm test`
+- `npm run ketchup`
 
-## Features
+Navigate Chrome to `http://localhost:3000`
 
-- Base React project with tests (Jest v.27)
-- Supports Axios and the browser's fetch API
-- Supports styling via Styled Components and vanilla CSS
-- Includes an Express server learners can interact with from the app or via Postman (http://localhost:9000)
-- Single `dev` script to launch the Express server and the auto-reloading dev server for the frontend
-- The `dev` script takes care of freeing ports 3000 and 9000 for better learner experience
-- The Express server is mocked in the tests with Mock Service Worker
-- It does not depend on any external API to fetch JSON
-- Easily deployable to Heroku, so learners can study a working prototype
-- Includes staff-facing [Codegrade setup instructions](codegrade_setup.md) and rubric
+## Endpoints
+
+- `GET http://localhost:9000/api/quotes`
+- `GET http://localhost:9000/api/quotes/:id`
+- `POST http://localhost:9000/api/quotes`
+- `UPDATE http://localhost:9000/api/quotes/:id`
+- `DELETE http://localhost:9000/api/quotes/:id`
--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -1,89 +1,9 @@
 import React from 'react'
-import axios from 'axios'
-import Form from './QuoteForm'
 
-export default class App extends React.Component {
-  state = {
-    quotes: [],
-    error: 'No errors :)',
-    authorInput: '',
-    textInput: '',
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:9000/api/quotes')
-      .then(res => {
-        this.setState({
-          ...this.state,
-          quotes: res.data.quotes
-        })
-      })
-      .catch(this.onError)
-  }
-
-  onError = (err) => {
-    this.setState({
-      ...this.state,
-      error: err.response.data.message,
-    })
-  }
-
-  onChange = (evt) => {
-    const { value, name } = evt.target
-    this.setState({
-      ...this.state,
-      [`${name}Input`]: value,
-    })
-  }
-
-  onSubmit = (evt) => {
-    evt.preventDefault()
-    const payload = { text: this.state.textInput, author: this.state.authorInput }
-    axios.post('http://localhost:9000/api/quotes', payload)
-      .then(res => {
-        this.setState({
-          ...this.state,
-          error: 'No errorz',
-          quotes: res.data.quotes,
-          authorInput: '',
-          textInput: '',
-        })
-      })
-      .catch(this.onError)
-  }
-
-  onDelete = id => () => {
-    axios.delete(`http://localhost:9000/api/quotes/${id}`)
-      .then(() => {
-        this.setState(state => ({
-          ...state,
-          quotes: state.quotes.filter(qo => qo.id != id)
-        }))
-      })
-      .catch(this.onError)
-  }
-
-  render() {
-    return (
-      <div>
-        <div id="error">Error: {this.state.error}</div>
-        quotes:
-        <ul>
-          {
-            !!this.state.quotes.length && this.state.quotes.map(quote => (
-              <li key={quote.id}>
-                <p><button onClick={this.onDelete(quote.id)}>delete</button>{quote.author} sayz {quote.text}</p>
-              </li>
-            ))
-          }
-        </ul>
-        <Form
-          onSubmit={this.onSubmit}
-          onChange={this.onChange}
-          authorInput={this.state.authorInput}
-          textInput={this.state.textInput}
-        />
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div>
+      <h2>Class Components</h2>
+    </div>
+  )
 }
