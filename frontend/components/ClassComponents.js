8193abb737909bc85488b09588e48bec6446bfb9
Ladrillo <gabriel@cabrejas.net>
2022-02-07 18:28:06 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function App() {
-  return (
-    <div>
-      <h2>Class Components</h2>
-    </div>
-  )
+export default class App extends React.Component {
+  render = () => {
+    return (
+      <div>
+        <h2>Class Components</h2>
+      </div>
+    )
+  }
 }
