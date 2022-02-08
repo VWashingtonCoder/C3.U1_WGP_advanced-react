67b36f7d9598b204c5139d19e4a3e12f27c598a8
Ladrillo <gabriel@cabrejas.net>
2022-02-08 15:21:39 +0100

    starting point

--- frontend/components/TheReactLifecycle.js
+++ frontend/components/TheReactLifecycle.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const StyledApp = styled.div`
-
-`
 
 export default function App() {
   return (
-    <StyledApp>
+    <div>
       <h2>The React Life Cycle</h2>
-    </StyledApp>
+    </div>
   )
 }
