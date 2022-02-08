de5d7809a027ffb4bbdd344a68cf84e0e3ec21b2
Ladrillo <gabriel@cabrejas.net>
2022-02-08 15:13:56 +0100

    tiny change server

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ server.get('*', (req, res) => {
 
 server.use((req, res) => {
   res.status(404).json({
-    message: `Endpoint [${req.method}] ${req.path} does not exist`,
+    message: `Endpoint [${req.method}] ${req.originalUrl} does not exist`,
   })
 })
 
