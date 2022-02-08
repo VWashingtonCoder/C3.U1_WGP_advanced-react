76a95622387a5f0aef9405e02cfdd63626880f83
Ladrillo <gabriel@cabrejas.net>
2022-02-08 18:34:34 +0100

    committing to "lecture"

--- frontend/components/TheReactLifecycle.js
+++ frontend/components/TheReactLifecycle.js
@@ -7,7 +7,8 @@ export default class App extends React.Component {
   }
   toggleChild = evt => {
     // this.state.renderChild = !this.state.renderChild // NEVAH
-    this.setState({ ...this.state, renderChild: true })
+    // this.setState({ ...this.state, renderChild: !this.state.renderChild })
+    this.setState(currState => )
   }
   render() {
     return (
