9a0a6a09e3a3146d85735b1b81ae2eaa3fc78b62
Ladrillo <gabriel@cabrejas.net>
2022-02-09 17:17:36 +0100

    ready for learners

---
+++ frontend/components/CustomHooks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <form>
+      <h2>Custom Hooks</h2>
+      <input name="foo" placeholder="type foo" />
+      <input name="bar" placeholder="type bar" />
+    </form>
+  )
+}
--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import ClassComponents from './components/ClassComponents'
 import TheReactLifecycle from './components/TheReactLifecycle'
-import UseEffectHook from './components/UseEffectHook'
+import CustomHooks from './components/CustomHooks'
 import TestingReact from './components/TestingReact'
 import './styles/reset.css'
 import './styles/styles.css'
@@ -15,13 +15,13 @@ render(
       <nav>
         <NavLink to="/">Class Components</NavLink>
         <NavLink to="/react-lifecycle">The React Life Cycle</NavLink>
-        <NavLink to="/use-effect-hook">The useEffect Hook</NavLink>
+        <NavLink to="/custom-hooks">Custom Hooks</NavLink>
         <NavLink to="/testing-react">Testing React</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<ClassComponents />} />
         <Route path="/react-lifecycle" element={<TheReactLifecycle />} />
-        <Route path="/use-effect-hook" element={<UseEffectHook />} />
+        <Route path="/custom-hooks" element={<CustomHooks />} />
         <Route path="/testing-react" element={<TestingReact />} />
       </Routes>
     </React.StrictMode>
