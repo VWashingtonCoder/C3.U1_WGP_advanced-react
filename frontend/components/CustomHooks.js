6ba404190cc6dc2488cff5d5d51fc6484ba6f3eb
Ladrillo <gabriel@cabrejas.net>
2022-02-09 18:57:27 +0100

    committing to "lecture"

--- frontend/components/CustomHooks.js
+++ frontend/components/CustomHooks.js
@@ -32,13 +32,14 @@ function useForm() {
 export default function App() {
   const [form, onChange] = useForm()
   const quotes = useQuotes()
+  debugger
   return (
     <form>
       <h2>Custom Hooks</h2>
       <input onChange={onChange} value={form.foo} name="foo" placeholder="type foo" />
       <input onChange={onChange} value={form.bar} name="bar" placeholder="type bar" />
       {
-        quotes.map && quotes.map(q => q.text)
+        quotes.map(q => q.text)
       }
     </form>
   )
