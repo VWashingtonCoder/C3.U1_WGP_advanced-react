6f0078b66cb3940c8011c8624dddabb8d55a354f
Ladrillo <gabriel@cabrejas.net>
2022-03-07 18:24:05 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -2,11 +2,7 @@ import React from 'react'
 import Form from './QuoteForm'
 
 export default class Quotes extends React.Component {
-  return (
-    <div>
-      <h2>Quotes:</h2>
-      <ul></ul>
-      <Form />
-    </div>
-  )
+  render() {
+    return null
+  }
 }
