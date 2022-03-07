26f48b37709dfa8851edb45f730a0ded2c06dbf6
Ladrillo <gabriel@cabrejas.net>
2022-03-07 17:21:10 +0100

    ok

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import axios from 'axios'
 import Form from './QuoteForm'
 
 export default function Quotes(props) {
   return (
     <div>
-      <h2>It is working</h2>
-      <div>Quotes:</div>
+      <h2>Quotes:</h2>
       <ul></ul>
       <Form />
     </div>
--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,13 +1,11 @@
 import React from 'react'
 
-export default class QuoteForm extends React.Component {
-  render() {
-    return (
-      <form onSubmit={this.props.onSubmit}>
-        <input onChange={this.props.onChange} value={this.props.textInput} type="text" id="textInput" placeholder="type text" />
-        <input onChange={this.props.onChange} value={this.props.authorInput} type="text" id="authorInput" placeholder="type author" />
-        <input type="submit" />
-      </form>
-    )
-  }
+export default function QuoteForm(props) {
+  return (
+    <form>
+      <input type="text" id="textInput" placeholder="type text" />
+      <input type="text" id="authorInput" placeholder="type author" />
+      <input type="submit" />
+    </form>
+  )
 }
