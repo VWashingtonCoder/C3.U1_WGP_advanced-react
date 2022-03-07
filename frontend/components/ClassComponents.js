e9371c2bfa84469a571ef0adcf635da8aa789fb3
Ladrillo <gabriel@cabrejas.net>
2022-03-07 19:41:30 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -42,6 +42,10 @@ export default class Quotes extends React.Component {
 
   }
 
+  addQuote = () => {
+
+  }
+
   render() { // traditional method syntax for functions that come with React
     console.log('props are', this.props)
     console.log('state is', this.state)
