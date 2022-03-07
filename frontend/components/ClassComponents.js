2e0683218d5663afddd0cd0f25ae8d0f0a9d5a9c
Ladrillo <gabriel@cabrejas.net>
2022-03-07 19:40:25 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -34,6 +34,12 @@ export default class Quotes extends React.Component {
     }) // drop a new state right in there
   }
 
+  capitalizeAuthor = id => {
+
+  }
+
+  
+
   render() { // traditional method syntax for functions that come with React
     console.log('props are', this.props)
     console.log('state is', this.state)
