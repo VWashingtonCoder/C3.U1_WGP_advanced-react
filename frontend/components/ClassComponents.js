342592a6daa0f129c284f667f5a0b83adbd3e073
Ladrillo <gabriel@cabrejas.net>
2022-03-07 18:30:49 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -5,7 +5,8 @@ let idx = 0
 const getIdx = () => ++idx
 
 const quotes = [
-  { id: getIdx(), author: 'Gabe'},
+  { id: getIdx(), author: 'Gabe', text: 'have fun with this' },
+  { id: getIdx(), author: 'Gabe', text: 'have fun with this' },
 ]
 
 export default class Quotes extends React.Component {
