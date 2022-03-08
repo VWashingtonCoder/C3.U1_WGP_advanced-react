7373f2d0ec215bc655ac234158a689d615581a77
Ladrillo <gabriel@cabrejas.net>
2022-03-08 18:31:00 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import Form from './QuoteForm'
 
-let idx = 0
-const getIdx = () => ++idx
-
-const quotes = [
-  { id: getIdx(), author: 'Gabe', text: 'have fun with this' },
-  { id: getIdx(), author: 'Paul', text: 'use Postman' },
-  { id: getIdx(), author: 'Anne', text: 'test your endpoints' },
-]
-
 const initialState = {
   errorMessage: '',
-  quotes,
+  quotes: ,
   form: {
     textInput: '',
     authorInput: '',
