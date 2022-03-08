e1af5886e267327da86cb57ba5a2bb6317286c2a
Ladrillo <gabriel@cabrejas.net>
2022-03-08 18:50:42 +0100

    committing to "lecture"

--- frontend/components/ClassComponents.js
+++ frontend/components/ClassComponents.js
@@ -15,11 +15,11 @@ const initialState = {
 }
 
 export default class Quotes extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = initialState
-  // }
-  state = initialState
+  constructor(props) {
+    super(props)
+    this.state = initialState
+  }
+  // state = initialState
 
   componentDidMount() {
     // this runs after the 1st dom surgery
