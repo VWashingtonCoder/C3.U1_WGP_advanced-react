8c2464b8dd0e8cb9b4646db3b9106a2e95c5d4d8
Ladrillo <gabriel@cabrejas.net>
2022-03-08 16:37:57 +0100

    ok

--- frontend/components/TheReactLifecycle.js
+++ frontend/components/TheReactLifecycle.js
@@ -1,5 +1,30 @@
 import React from 'react'
 
-export default function () {
-  return null
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { renderChild: false }
+  }
+  toggleChild = () => { }
+  render() {
+    return (
+      <div>
+        <h2>The React Life Cycle</h2>
+        <button onClick={this.toggleChild}>toggle child</button>
+        {this.state.renderChild && <TheChild lady='gaga' />}
+      </div>
+    )
+  }
+}
+
+class TheChild extends React.Component {
+  render() {
+    console.log('👉 component rendering')
+    return (
+      <div>
+        <h3>The count is 0</h3>
+        <button onClick={Function.prototype}>increment</button>
+      </div>
+    )
+  }
 }
