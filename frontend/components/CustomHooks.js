e7a1e5924c38fe07b033ca9a6d8a737e9a4717a8
Ladrillo <gabriel@cabrejas.net>
2022-03-09 18:17:01 +0100

    committing to "lecture"

--- frontend/components/CustomHooks.js
+++ frontend/components/CustomHooks.js
@@ -8,12 +8,15 @@ export function useForm(values) {
   return [form, onChange]
 }
 
+
+
 export default function App() {
   const [form, onChange] = useForm({ foo: '', bar: '', baz: '' })
   const quoteOfTheDay = useRandomQuote()
+
   return (
     <form>
-      <h2>Custom Hooks</h2>
+      <h2>Custom Hooks {quoteOfTheDay}</h2>
       <input onChange={onChange} value={form.foo} name="foo" placeholder="type foo" />
       <input onChange={onChange} value={form.bar} name="bar" placeholder="type bar" />
       <input onChange={onChange} value={form.baz} name="baz" placeholder="type baz" />
