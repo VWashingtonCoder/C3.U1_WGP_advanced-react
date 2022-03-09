9c1091374c42595dc07f35e23a72f08978637310
Ladrillo <gabriel@cabrejas.net>
2022-03-09 16:12:00 +0100

    prep

--- frontend/components/CustomHooks.js
+++ frontend/components/CustomHooks.js
@@ -1,5 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function () {
-  return null
+export default function App() {
+  const [form, setForm] = useState({ foo: '', bar: '', baz: '' })
+  const onChange = ({ target: { name, value } }) =>
+    setForm({ ...form, [name]: value  })
+
+  return (
+    <form>
+      <h2>Custom Hooks</h2>
+      <input onChange={onChange} value={form.foo} name="foo" placeholder="type foo" />
+      <input onChange={onChange} value={form.bar} name="bar" placeholder="type bar" />
+      <input onChange={onChange} value={form.baz} name="baz" placeholder="type baz" />
+    </form>
+  )
 }
