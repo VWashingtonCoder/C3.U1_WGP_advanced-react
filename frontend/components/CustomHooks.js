696bd9434d33dc58466d97ba28b15254204690d8
Ladrillo <gabriel@cabrejas.net>
2022-03-09 19:07:19 +0100

    committing to "lecture"

--- frontend/components/CustomHooks.js
+++ frontend/components/CustomHooks.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export function useLocalStorage(key, value) {
-  const []
+  const [valueLS, setValueLS] = useState(() => {
+    
+  })
 }
 
 export function useForm(values) {
