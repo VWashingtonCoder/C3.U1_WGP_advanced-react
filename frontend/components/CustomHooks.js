dadc5d1fcd8ea67a0556bf01708144feb8f13b2f
Ladrillo <gabriel@cabrejas.net>
2022-03-09 19:30:49 +0100

    committing to "lecture"

--- frontend/components/CustomHooks.js
+++ frontend/components/CustomHooks.js
@@ -43,10 +43,12 @@ export function useRandomQuote() {
 
 export default function App() {
   const [form, onChange] = useForm({ foo: '', bar: '', baz: '' })
+  const [count, setCount] = useLocalStorage(0)
   const quoteOfTheDay = useRandomQuote()
 
   return (
     <form>
+      <h1>{count}</h1>
       <h2>Custom Hooks {quoteOfTheDay}</h2>
       <input onChange={onChange} value={form.foo} name="foo" placeholder="type foo" />
       <input onChange={onChange} value={form.bar} name="bar" placeholder="type bar" />
