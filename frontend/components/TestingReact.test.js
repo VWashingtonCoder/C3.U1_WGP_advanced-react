baae2cb99d219f8c65551b35c4382d7705ebc1e3
Ladrillo <gabriel@cabrejas.net>
2022-03-10 18:50:01 +0100

    committing to "lecture"

--- frontend/components/TestingReact.test.js
+++ frontend/components/TestingReact.test.js
@@ -20,7 +20,7 @@ describe('App component', () => {
   })
   test('Renders heading "Todos:"', () => {
     // const heading = document.querySelector('h2') // not like this
-    const heading = screen.queryByText('TodoZ:')
+    const heading = screen.queryByText('Todos:')
     console.log(heading)
   })
 })
