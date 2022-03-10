1d9c4a8c676714badfca4d4a99ba3e163986a034
Ladrillo <gabriel@cabrejas.net>
2022-03-10 19:55:54 +0100

    committing to "lecture"

--- frontend/components/TestingReact.test.js
+++ frontend/components/TestingReact.test.js
@@ -72,10 +72,11 @@ describe('App component', () => {
     expect(input).toHaveValue('ab')
   })
   test('we can submit a new todo and it renders to the screen', async () => {
+    expect(todoInput()).toHaveValue('')
     fireEvent.change(todoInput(), { target: { value: 'Have LOTS of fun' } })
     expect(todoInput()).toHaveValue('Have LOTS of fun')
     fireEvent.click(submitBtn())
     await screen.findByText('Have LOTS of fun')
-    expect(todoInput()).toHaveValue('Have LOTS of fun')
+    expect(todoInput()).toHaveValue('')
   })
 })
