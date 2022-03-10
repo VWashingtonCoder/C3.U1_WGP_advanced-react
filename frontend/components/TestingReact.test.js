963c6d2220599c7bb00c82087d5cac94164ec52a
Ladrillo <gabriel@cabrejas.net>
2022-03-10 18:21:21 +0100

    committing to "lecture"

--- frontend/components/TestingReact.test.js
+++ frontend/components/TestingReact.test.js
@@ -3,4 +3,6 @@ import { render, fireEvent, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import App from './TestingReact'
 
-
+beforeEach(() => {
+  render(<App /> )
+})
