50cec1c001f1a71acdbe92599f44d107e5d31306
Ladrillo <gabriel@cabrejas.net>
2022-03-10 19:30:38 +0100

    committing to "lecture"

--- frontend/components/TestingReact.test.js
+++ frontend/components/TestingReact.test.js
@@ -53,5 +53,6 @@ describe('App component', () => {
   test('submit button is disabled until we type one char', () => {
     const submitBtn = screen.getByText('Submit Todo')
     expect(submitBtn).toBeDisabled()
+    fireEvent.change()
   })
 })
